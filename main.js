c148e7fda7bc1f0688095a2e8b7e8eb533c9df36
Alesya103 <alesya.afanaseva27@gmail.com>
2020-01-29 01:15:34 +0200

    finally done

--- main.js
+++ main.js
@@ -1,24 +1,18 @@
 Vue.component('item', {
-  props: ['data'],
+  props: ['item'],
   methods: {
     delete_item() {
       this.$emit('delete_item');
     },
   },
-  template:
-        `<li class="item" :class="{checked:data.completed} ">
-            <input type="checkbox" id="checkbox" v-model="data.completed">
-            <h6 class="col-xs col-sm-10 item__text">{{data.text}}</h6>
-            <h6 class="item__text">{{data.priority}}</h6>
+  template: `<li class="item" :class="{checked:item.completed}" >
+            <input type="checkbox" id="checkbox" v-model="item.completed">
+            <h6 class="col-xs col-sm-10 item__text">{{item.text}}</h6>
+            <h6 class="item__text">{{item.priority}}</h6>
             <button @click="delete_item" class="btn btn-danger" type="submit">
             <i class="fas fa-trash"></i>
             </button>
         </li>`,
-  // data : function(){
-  //   return{
-
-  //   }
-  // }
 });
 const app = new Vue({
   el: '#app',
@@ -27,9 +21,11 @@ const app = new Vue({
     tittle: 'ToDo',
     newItem: {
       text: '',
-      priority: '',
-      completed: false
+      priority: '0',
+      completed: false,
     },
+    status: 'All',
+    priority: 0,
     items: [],
   },
   methods: {
@@ -38,12 +34,22 @@ const app = new Vue({
         this.items.push({
           text: this.newItem.text,
           priority: this.newItem.priority,
+          completed: this.newItem.completed,
         });
         this.newItem.text = '';
         this.newItem.priority = '';
       }
     },
-    
+    orderFilter(order) {
+      if(order == 'Ascending') {
+        this.items = this.items.sort((a, b) => +a.priotity > +b.priotity ? 1: -1);
+      } else if (order == 'Descending') {
+        this.items = this.items.sort((a, b) => +b.priotity < +a.priotity ? 1: -1);
+      }
+    },
+    itemStatus(status) {
+      this.status = status
+    },
     deleteThisItem(index) {
       this.items.splice(index, 1);
     },
@@ -52,8 +58,18 @@ const app = new Vue({
     progress() {
       return this.items.length > 0 ? Math.round(this.tasksDone / this.items.length * 100) : 0;
     },
-    tasksDone(){
-      return this.items.filter(item => item.completed).length;
+    tasksDone() {
+      return this.items.filter((item) => item.completed).length;
+    },
+    visibleItems() {
+      let arr = [];
+      arr = this.items.filter(el => el.text.toLowerCase().includes(this.search))
+      if (this.status == 'Done') {
+        arr = arr.filter(el => el.completed);
+      } else if (this.status == 'Undone') {
+        arr = arr.filter(el => !el.completed);
+      }
+      return arr;
     }
   },
 });
